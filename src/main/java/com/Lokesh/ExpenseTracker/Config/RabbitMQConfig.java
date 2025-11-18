1e45dda65b4305e309a94dce4e6bb7e00780ef6b
Lokesh7475 <142487744+Lokesh7475@users.noreply.github.com>
2025-11-18 16:58:51 +0530

    Fixed RabbitMQConfig, added message converter, rabbit template and simple rabbit listener container factory.

--- src/main/java/com/Lokesh/ExpenseTracker/Config/RabbitMQConfig.java
+++ src/main/java/com/Lokesh/ExpenseTracker/Config/RabbitMQConfig.java
@@ -1,6 +1,10 @@
 package com.Lokesh.ExpenseTracker.Config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,5 +15,25 @@ public class RabbitMQConfig {
     @Bean
     public Queue emailQueue(){
         return new Queue(EMAIL_QUEUE);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter jackson2JsonMessageConverter){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter);
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter jackson2JsonMessageConverter){
+        SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory = new SimpleRabbitListenerContainerFactory();
+        rabbitListenerContainerFactory.setConnectionFactory(connectionFactory);
+        rabbitListenerContainerFactory.setMessageConverter(jackson2JsonMessageConverter);
+        return rabbitListenerContainerFactory;
     }
 }
