5876bd826c8b42c20815548d9d63d9afc5505330
Lokesh7475 <142487744+Lokesh7475@users.noreply.github.com>
2025-11-22 18:42:30 +0530

    Added CacheConfig for redis

--- src/main/java/com/Lokesh/ExpenseTracker/Config/CacheConfig.java
+++ src/main/java/com/Lokesh/ExpenseTracker/Config/CacheConfig.java
@@ -1,7 +1,25 @@
 package com.Lokesh.ExpenseTracker.Config;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.time.Duration;
 
 @EnableCaching
 public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .prefixCacheNameWith("expense-tracker-")
+                .entryTtl(Duration.ofHours(1))
+                .enableTimeToIdle();
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
 }
